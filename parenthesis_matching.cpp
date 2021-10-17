c02d95ff33994a4915f059444efaf24a7fbc5de3
Tanya Singh <tanya.singh.1611@gmail.com>
2021-10-17 14:28:38 +0530

    Updated paranthesis_matching.cpp

--- parenthesis_matching.cpp
+++ parenthesis_matching.cpp
@@ -122,3 +122,68 @@ int main()
 		cout<<"Not Balanced\n";
 }
 
+//using stack.h
+#include<iostream>
+using namespace std;
+#include "stack.h"
+
+bool balanced(string exp)
+{
+  Stack <char> s;
+  //bool res = false;
+  int l = exp.length();
+  for(int i=0; i<l; i++)
+  {
+    if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+      s.push(exp[i]);
+    else if(exp[i] == ')')
+    {
+      if(s.top_ele() == '(')
+      {
+        s.pop();
+        //res = true;
+      }
+      else
+        return false;
+    }
+    else if(exp[i] == ']')
+    {
+      if(s.top_ele() == '[')
+      {
+        s.pop();
+        //res = true;
+      }
+      else
+        return false;
+    }
+    else if(exp[i] == '}')
+    {
+      if(s.top_ele() == '{')
+      {
+        s.pop();
+        //res = true;
+      }
+      else
+        return false;
+    }
+  }
+  if(s.is_empty() == 0)
+    return true;
+  else
+    return false;
+  //return res;
+}
+
+int main()
+{
+  string exp;
+  cout<<"Enter the expression: ";
+  cin>>exp;
+
+  if(balanced(exp) == 1)
+    cout<<"Equation is balanced!"<<endl;
+  else
+    cout<<"Equation is not balanced!"<<endl;
+
+  return 0;
+}
